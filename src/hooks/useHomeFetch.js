964174dd1623e96200304a43b9e3c1f49d380cac
saish gadekar <saish.gadekar.58@gmail.com>
2021-08-21 05:52:26 +0530

    update21.8

--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 //API
 import API from "../API";
+///helpers
+import { isPersistedState } from "../helpers";
 
 const initialState = {
   page: 0,
@@ -36,6 +38,15 @@ export const useHomeFetch = () => {
   };
   //initial render and search
   useEffect(() => {
+    if (!searchTerm) {
+      const sessionState = isPersistedState("homeState");
+
+      if (sessionState) {
+        setState(sessionState);
+        return;
+      }
+    }
+
     setState(initialState);
     fetchMovies(1, searchTerm);
   }, [searchTerm]);
@@ -46,6 +57,11 @@ export const useHomeFetch = () => {
     fetchMovies(state.page + 1, searchTerm);
     setIsLoadingMore(false);
   }, [isLoadingMore, searchTerm, state.page]);
+
+  //write to session storage
+  useEffect(() => {
+    if (!searchTerm) sessionStorage.setItem("homeState", JSON.stringify(state));
+  }, [searchTerm, state]);
 
   return {
     state,
--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 //api
 import API from "../API";
 //
-
+import { isPersistedState } from "../helpers";
 export const useMovieFetch = (movieId) => {
   const [state, setState] = useState({});
   const [loading, setLoading] = useState(true);
@@ -31,8 +31,22 @@ export const useMovieFetch = (movieId) => {
         setLoading(false);
       }
     };
+
+    const sessionState = isPersistedState(movieId);
+
+    if (sessionState) {
+      setState(sessionState);
+      setLoading(false);
+      return;
+    }
+
     fetchMovie();
   }, [movieId]);
+
+  //write to ss
+  useEffect(() => {
+    sessionStorage.setItem(movieId, JSON.stringify(state));
+  }, [movieId, state]);
 
   return { state, loading, error };
 };
